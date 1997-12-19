011a29619879d28fd6c044407a3d12be32372316
houghton <>
1997-12-19 12:48:22 +0000

    Cleanup

--- SMKCommon/SMKDateTimeUtils.h
+++ SMKCommon/SMKDateTimeUtils.h
@@ -2,46 +2,29 @@
 #define _DateTimeUtils_hh_
 //
 // File:        DateTimeUtils.hh
+// Project:	StlUtils
 // Desc:        
 //
 //
 //
-// Author:      Paul Houghton - (houghton@cworld.wiltel.com)
+// Quick Start: - short example of class usage
+//
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
 // Created:     05/02/95 06:32
 //
-// Revision History:
+// Revision History: (See end of file for Revision Log)
 //
-// $Log$
-// Revision 4.1  1997/09/17 15:12:21  houghton
-// Changed to Version 4
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
 //
-// Revision 3.3  1997/09/17 11:08:18  houghton
-// Changed: renamed library to StlUtils.
-//
-// Revision 3.2  1997/08/24 23:07:04  houghton
-// Changed type of global const values to time_t.
-//
-// Revision 3.1  1996/11/14 01:23:39  houghton
-// Changed to Release 3
-//
-// Revision 2.2  1996/04/27 12:59:36  houghton
-// Cleanup
-//
-// Revision 2.1  1995/11/10 12:40:33  houghton
-// Change to Version 2
-//
-// Revision 1.3  1995/11/05  15:28:32  houghton
-// Revised
-//
+//  $Id$
 //
 
-#if !defined( STLUTILS_SHORT_FN )
 #include <StlUtilsConfig.hh>
 #include <ctime>
-#else
-#include <StlUtilsCfg.hh>
-#include <ctime>
-#endif
+#include <cstring>
+
 
 extern const time_t STLUTILS_GVAR_T SecPerMin;
 extern const time_t STLUTILS_GVAR_T SecPerHour;
@@ -79,11 +62,48 @@ typedef enum {
 
 #endif
 
-#if !defined( STLUTILS_SHORT_FN )
 #include <DateTimeUtils.ii>
-#else
-#include <DtTmUtil.ii>
-#endif
 
+//
+// Detail Documentation
+//
+// Example:
+//
+// See Also:
+//
+// Files:
+//
+// Documented Ver:
+//
+// Tested Ver:
+//
+// Revision Log:
+//
+// $Log$
+// Revision 4.2  1997/12/19 12:48:22  houghton
+// Cleanup
+//
+// Revision 4.1  1997/09/17 15:12:21  houghton
+// Changed to Version 4
+//
+// Revision 3.3  1997/09/17 11:08:18  houghton
+// Changed: renamed library to StlUtils.
+//
+// Revision 3.2  1997/08/24 23:07:04  houghton
+// Changed type of global const values to time_t.
+//
+// Revision 3.1  1996/11/14 01:23:39  houghton
+// Changed to Release 3
+//
+// Revision 2.2  1996/04/27 12:59:36  houghton
+// Cleanup
+//
+// Revision 2.1  1995/11/10 12:40:33  houghton
+// Change to Version 2
+//
+// Revision 1.3  1995/11/05  15:28:32  houghton
+// Revised
+//
+//
 #endif // ! def _DateTimeUtils_hh_ 
 
