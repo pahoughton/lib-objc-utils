c26d18eb285b8c4a5af39951c826527302c9e7e6
houghton <>
1997-12-20 16:07:58 +0000

    Initial Version.

---
+++ SMKCommon/SMKStripTrailing.cpp
@@ -0,0 +1,48 @@
+//
+// File:        StripTrailing.C
+// Project:	StlUtils
+// Desc:        
+//
+//  Compiled sources for StripTrailing
+//  
+// Author:      Paul Houghton - (paul.houghton@wcom.com)
+// Created:     12/20/97 08:02
+//
+// Revision History: (See end of file for Revision Log)
+//
+//  Last Mod By:    $Author$
+//  Last Mod:	    $Date$
+//  Version:	    $Revision$
+//
+
+#include "StringUtils.hh"
+
+STLUTILS_FUNCT_VERSION(
+  StripTrailing,
+  "$Id$" );
+
+
+char *
+StripTrailing( char * buffer, const char * white, size_t bufferSize )
+{
+  long      len = ( bufferSize == NPOS ? strlen( buffer ) : bufferSize );
+  long	    pos = len;
+  
+  for( ; pos >= 0 ; -- pos )
+    if( ! strchr( white, buffer[pos] ) )
+      break;
+
+  if( pos < len )
+    buffer[ pos + 1 ] = 0;
+  
+  return( buffer );
+}
+
+
+// Revision Log:
+//
+// $Log$
+// Revision 1.1  1997/12/20 16:07:58  houghton
+// Initial Version.
+//
+//
