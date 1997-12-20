3d16dad46a8e9da14b573c3154750851b8515eab
houghton <>
1997-12-20 16:08:33 +0000

    Added Detail descriptions.

--- SMKCommon/SMKDateTimeUtils.h
+++ SMKCommon/SMKDateTimeUtils.h
@@ -67,19 +67,120 @@ typedef enum {
 //
 // Detail Documentation
 //
+// Types:
+//
+//	enum DayOfWeek
+//	    The days of the week where Sunday is 0 and Saturday is 6
+//
+// Variables:
+//
+//	extern const time_t STLUTILS_GVAR_T SecPerMin;
+//	    The number of seconds in a minute (60)
+//
+//	extern const time_t STLUTILS_GVAR_T SecPerHour;
+//	    The number of seconds in a hour (60 * 60)
+//
+//	extern const time_t STLUTILS_GVAR_T SecPerDay;
+//	    The number of seconds in a day (60 * 60 * 24)
+//
+//	extern const time_t STLUTILS_GVAR_T SecPerYear;
+//	    The number of seconds in a year (60 * 60 * 24 * 365)
+//	
+//	extern const time_t STLUTILS_GVAR_T MinPerHour;
+//	    The number of minutes in a hour (60)
+//
+//	extern const time_t STLUTILS_GVAR_T MinPerDay;
+//	    The number of minutes in a day (60 * 24)
+//	
+//	extern const time_t STLUTILS_GVAR_T MinTimeT;
+//	    The minimum time_t value
+//
+//	extern const time_t STLUTILS_GVAR_T MaxTimeT;
+//	    The maximum time_t value
+//	
+//	extern const time_t STLUTILS_GVAR_T MinYear;
+//	    The minimum year possible for a time_t value
+//
+//	extern const time_t STLUTILS_GVAR_T MaxYear;
+//	    The maximum year possible for a time_t value
+//
+//	extern const int    STLUTILS_GVAR_T DaysInMonth[];
+//	    The number of days in each month where Jan is month 0 so,
+//	    DaysInMonth[1] == 28.
+//
+//	extern const int    STLUTILS_GVAR_T MonthDayOfYear[];
+//	    The number of days since the beging of the year for the first day
+//	    of the month where Jan is month 0 so
+//	    MonthDayOfYear[1] == 31 && MonthDayOfYear[2] == 58
+//
+//	extern const char * STLUTILS_GVAR_T Months[];
+//	    The full names of the months where Jan is month 0 so,
+//	    Months[2] == "March"
+//
+//	extern const char * STLUTILS_GVAR_T AbbrMonths[];
+//	    The first three letters of the month where Jan is month 0 so,
+//	    AbbrMonths[9] == "Oct"
+//
+//	extern const char * STLUTILS_GVAR_T WeekDays[];
+//	    The full name of the days of the week where sunday is 0 so,
+//	    WeekDays[ 2 ] = "Tuesday"
+//
+//	extern const char * STLUTILS_GVAR_T AbbrWeekDays[];
+//	    The 3 char abbeviated names of the days of the week where
+//	    sunday is 0, so AbbrWeekDays[ 4 ] == "Thu"
+//
+//	
+// Functions:
+//
+//  	long
+//  	SecInTimeT( long sec );
+//  	    return the second of the minute.
+//
+//  	long
+//  	MinInTimeT( long sec )
+//  	    return the minute of the hour.
+//
+//  	long
+//  	HourInTimeT( long sec )
+//  	    return the hour of the day.
+//
+//  	long
+//  	HoursInTimeT( long sec )
+//  	    return the number of hours.
+//
+//  	long
+//  	DaysInTimeT( long sec )
+//  	    return the number of days.
+//
+//  	long
+//  	DateInTimeT( long sec )
+//  	    return the date portion only.
+//
+//	void
+//	YYMMDDtoYYYYMMDD( char * dest, const char * src )
+//	    convert 970304 to 19970304. if the first digit is < 5
+//	    the year is assumed to be 2000, so 050304 will be 20050304.
+//
 // Example:
 //
 // See Also:
 //
 // Files:
 //
-// Documented Ver:
+//	DateTimeUtils.hh
+//	DateTimeUtils.ii
+//	libStlUtils.a
 //
-// Tested Ver:
+// Documented Ver: 4.3
+//
+// Tested Ver: 4.3
 //
 // Revision Log:
 //
 // $Log$
+// Revision 4.3  1997/12/20 16:08:33  houghton
+// Added Detail descriptions.
+//
 // Revision 4.2  1997/12/19 12:48:22  houghton
 // Cleanup
 //
